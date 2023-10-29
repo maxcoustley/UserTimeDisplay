32ca009cd22cb62ecb54da2fa71eebd16c890b68
maxcoustley <148026491+maxcoustley@users.noreply.github.com>
2023-10-29 15:11:47 +1100

    fixed timezone + date and time seperated

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 console.log(dayjs())
 document.getElementById("time").innerHTML = `
-    <h3>${dayjs().toISOString()}</h3>
+    <h1>Time: ${dayjs().format('HH:mm:ss')}</h1>
+`
+dayjs.extend(dayjs_plugin_timezone);
+dayjs.extend(dayjs_plugin_utc);
+
+document.getElementById("timezone").innerHTML = `
+    <h3>Timezone: ${dayjs.tz.guess()}</h3>
 `
 
-const userTimezone = dayjs.tz.guess();
-console.log("User's Timezone: ");
-document.getElementById("timezone").innerHTML = `
-    <h3>${dayjs.tz.guess().toISOString()}</h3>
+document.getElementById("date").innerHTML = `
+    <h3>Date: ${dayjs().format('YYYY-MM-DD')}</h3>
 `
 
 
